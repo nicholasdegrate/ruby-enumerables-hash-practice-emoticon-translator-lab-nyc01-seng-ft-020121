e08137922faa4a94a12ae6749f395f7420521126
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-28 23:03:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,16 +3,34 @@ require "yaml"
 file = YAML.load_file("emotionicons.yml")
 puts file.inspect
 
+require 'yaml'
+
 def load_library(file)
-  
-  
-  # code goes here
+  new_file =  YAML.load_file('emotionicons.yml')
+  icons = new_file.each_with_object({}) do |(k, v), icons|
+    icons[k] = Hash[:English,v[0], :Japanese, v[1]]
+  end 
+  icons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file,j_emotions)
+  emotions = load_library(file)
+  result = ''
+  emotions.each do |meaning, icons|
+    if j_emotions == icons[:Japanese]
+      result = meaning
+    end
+  end
+  puts result
 end
 
-def get_english_meaning
-  # code goes here
+def get_japanese_emoticon(file, w_emoticon)
+  emoticons = load_library(file)
+  result = "Sorry, that emoticon was not found"
+  emoticons.each do |meaning, emotes|
+    if w_emoticon == emotes[:english]
+      result = emotes[:english]
+    end 
+  end 
+  result
 end
